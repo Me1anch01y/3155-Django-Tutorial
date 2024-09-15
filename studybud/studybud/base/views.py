3016949e8e9fa89584bcce0dc6fe792ea9637ff3
Me1anch01y <johnksmith8@gmail.com>
2024-09-15 18:19:50 -0400

    User Logout + Restricted Pages

--- studybud/studybud/base/views.py
+++ studybud/studybud/base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
 # Create your views here.
@@ -19,9 +22,23 @@ def loginPage(request):
         try:
             user = User.objects.get(username=username)
         except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exist')
 
     context = {}
     return render(request, 'base/login_register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 
 def home(request):
@@ -46,6 +63,7 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 
+@login_required()
 def createRoom(request):
     form = RoomForm()
     if request.method == 'POST':
